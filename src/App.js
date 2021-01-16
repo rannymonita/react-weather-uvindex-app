e8859c65d03ce4dd7d82b4d70ff18bcd45e4ff32
rannymonita <rannymonita@gmail.com>
2021-01-16 19:25:36 +0000

    Added Bootstrap & assets

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import logo from "./logo.svg";
+import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1> Weather App </h1>
-      <footer>
-        This project was coded by Ranny Monita and is open-sourced on{" "}
-        <a
-          href="https://github.com/rannymonita/react-weather-uvindex-app"
-          target="_blank"
-        >
-          Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1> Weather App </h1>
+        <Weather />
+
+        <footer>
+          This project was coded by Ranny Monita and is open-sourced on{" "}
+          <a
+            href="https://github.com/rannymonita/react-weather-uvindex-app"
+            target="_blank"
+          >
+            Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
