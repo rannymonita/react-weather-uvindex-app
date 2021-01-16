fdbb50b8214129528bef7f3e1bb596fb67292502
rannymonita <rannymonita@gmail.com>
2021-01-16 16:44:09 +0000

    Adding Github repo URL

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <p> Weather App </p>
+      <h1> Weather App </h1>
+      <footer>
+        This project was coded by Ranny Monita and is open-sourced on{" "}
+        <a
+          href="https://github.com/rannymonita/react-weather-uvindex-app"
+          target="_blank"
+        >
+          Github
+        </a>
+      </footer>
     </div>
   );
 }
-
-export default App;
